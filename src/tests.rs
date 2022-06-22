7ad30ce5d97857d3dae5f625fbb6296d3fcab3d0
Benoît du Garreau <bdgdlm@outlook.com>
2022-06-22 14:46:30 +0200

    Initial commit

---
+++ src/tests.rs
@@ -0,0 +1,61 @@
+use crate::*;
+use std::{thread, time};
+
+#[test]
+fn smoke_test() {
+    let store = OnceMap::new();
+    let val = store.insert_with(String::from("aaa"), |_| String::from("bbb"));
+    assert_eq!(val, store.get("aaa").unwrap());
+}
+
+#[test]
+fn concurrent_init() {
+    let store = OnceMap::new();
+    let count = Mutex::new(0);
+
+    crossbeam_utils::thread::scope(|s| {
+        s.spawn(|_| {
+            thread::sleep(time::Duration::from_millis(50));
+            store.insert_with(String::from("aaa"), |_| {
+                thread::sleep(time::Duration::from_millis(50));
+                *count.lock() += 1;
+                String::from("bbb")
+            })
+        });
+
+        s.spawn(|_| {
+            thread::sleep(time::Duration::from_millis(50));
+            store.insert_with(String::from("aaa"), |_| {
+                thread::sleep(time::Duration::from_millis(50));
+                *count.lock() += 1;
+                String::from("bbb")
+            })
+        });
+
+        store
+            .try_insert_with(String::from("aaa"), |_| {
+                thread::sleep(time::Duration::from_millis(200));
+                *count.lock() += 2;
+                Err(())
+            })
+            .unwrap_err();
+    })
+    .unwrap();
+
+    assert_eq!(*count.lock(), 3);
+    assert_eq!(store.get("aaa").unwrap(), "bbb");
+}
+
+#[test]
+fn reentrant_init() {
+    let store = OnceMap::with_single_shard();
+
+    let res = store.insert_with(String::from("aaa"), |_| {
+        let x = store.insert_with_cloned(String::from("bbb"), |_| String::from("x"));
+        let y = store.insert_with(String::from("ccc"), |_| String::from("y"));
+        assert!(store.get("aaa").is_none());
+        x + y
+    });
+
+    assert_eq!(res, "xy");
+}
